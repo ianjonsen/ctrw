31c7f6ab072fbd6324612b630bdb84d1399e5ede
ianjonsen <ian.jonsen@mq.edu.au>
2018-06-19 12:26:00 -0300

    merge lgl's in prefilter output, added test data

--- R/prefilter.r
+++ R/prefilter.r
@@ -1,5 +1,5 @@
 prefilter <- function(d, time.gap = 21) {
-  
+
   ##  convert dates to POSIXt
   ##  remove any duplicate time records,
   ##  order records by time,
@@ -21,8 +21,8 @@ prefilter <- function(d, time.gap = 21) {
       na.action = "na.exclude",
       control = loess.control(surface = "direct")
     ) %>%
-    .$residuals 
- 
+    .$residuals
+
   res.lat <-
     loess(
       lat ~ as.numeric(date),
@@ -31,33 +31,37 @@ prefilter <- function(d, time.gap = 21) {
       na.action = "na.exclude",
       control = loess.control(surface = "direct")
     ) %>%
-    .$residuals 
-  
+    .$residuals
+
  ## flag extreme outlier locations to be ignored by SSM filter
- d <- d %>% 
-    mutate(dist.keep = ifelse((res.lon > quantile(res.lon, 0.001) & res.lon < quantile(res.lon, 0.999)) & 
+ d <- d %>%
+    mutate(dist.keep = ifelse((res.lon > quantile(res.lon, 0.001) & res.lon < quantile(res.lon, 0.999)) &
            (res.lat > quantile(res.lat, 0.001) & res.lat < quantile(res.lat, 0.999)), TRUE, FALSE))
- 
+
  ## convert ellipse orientation from deg to rad
  ## convert lon from 0,360 to -180,180
  ## convert coords from lonlat to mercator (km)
  ## convert ellipse axes from m to km
- d <- d %>% 
+ d <- d %>%
    mutate(eor = eor / 180 * pi) %>%
-   mutate(lon = ifelse(lon > 180, 180 - lon, lon)) 
-  
+   mutate(lon = ifelse(lon > 180, 180 - lon, lon))
+
  ## identify time gaps (in days) between observations
  d <- d %>%
    mutate(diff = (date - lag(date)) / 86400) %>%
    mutate(time.keep = TRUE)
- 
- 
+
+
  ## flag segments after long time gaps
  q <- which(d$diff > time.gap)
  if(length(q) > 0) {
    q <- min(q)
    d$time.keep[q:nrow(d)] <- FALSE
  }
+
+ ## merge .keep lgl vectors
+ d <- d %>%
+   mutate(keep = ifelse((dist.keep | time.keep), TRUE, FALSE))
 
  d %>% select(id, date, lc, lon, lat, smaj, smin, eor, dist.keep, time.keep)
 }
