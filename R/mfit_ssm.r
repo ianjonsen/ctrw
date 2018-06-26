34eaab6e5dace3c3b2bfe73f913a3474870f032f
ianjonsen <ian.jonsen@mq.edu.au>
2018-06-26 09:24:55 -0300

    add args for prefilter

--- R/mfit_ssm.r
+++ R/mfit_ssm.r
@@ -3,16 +3,18 @@
 ##' Fit ssm to multiple individual tracks
 ##'
 ##' @param d input data.frame
-##' @param ... arguments to be passed to prefilter and fit_ssm
+##' @param span degree of loess smoothing (range: 0 - 1) to identify potential outliers in prefilter
+##' @param min.dist minimum distance from track to define potential outlier locations
+##' @param ... arguments to be passed to fit_ssm
 ##' @importFrom dplyr group_by do rowwise %>%
 ##'
 ##' @export
-mfit_ssm <- function(d, ...)
+mfit_ssm <- function(d, span = 0.01, min.dist = 100, ...)
 {
 
   fit <- d %>%
     group_by(id) %>%
-    try(do(pf = prefilter(., ...)), silent = TRUE) %>%
+    try(do(pf = prefilter(., span = span, min.dist = min.dist)), silent = TRUE) %>%
     rowwise() %>%
     do(ssm = fit_ssm(.$pf, ...))
 
